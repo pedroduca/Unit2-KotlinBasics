4ee88c90fad4628510a390b3b34b3d70aba1f16c
Pedro Duca <testandoduca@gmail.com>
2023-11-19 19:36:42 -0300

    Creating UnitTest

--- app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
+++ app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
@@ -1,17 +1,16 @@
 package com.example.diceroller
 
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
-import org.junit.Assert.*
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ExampleUnitTest {
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun generates_number() {
+        val dice = Dice(6)
+        val rollResult = dice.roll()
+
+        assertTrue("O valor de rollResut não é entre 1 e 6", rollResult in 1..6)
     }
 }
